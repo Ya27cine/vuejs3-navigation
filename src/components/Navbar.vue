8571b5f6e99598a7a5338955b658532de3415588
KHELIFA Yassine <prostam27@gmail.com>
2021-09-28 22:59:53 +0200

    Router Link Active

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">VueJs</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -35,6 +35,13 @@ export default {
     
 }
 </script>
-<style lang="">
-    
+<style>
+
+.router-link-active{
+  color: black !important;
+  background: white;
+  padding: 6px;
+}
+
+
 </style>
