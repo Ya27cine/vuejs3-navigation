00258987b9fa82239296dcb8768d5345e6ed381e
KHELIFA Yassine <prostam27@gmail.com>
2021-10-01 00:10:08 +0200

    Retreive One Post

--- src/views/blog/Show.vue
+++ src/views/blog/Show.vue
@@ -1,20 +1,29 @@
 <template>
 
 
-    <h1> Show Post ID : {{ id }}</h1>
-    <h2> Slug : {{ slug }}</h2>
+    <h1> {{ post.title}}</h1>
 
-    <p class="lead"> Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, eligendi ea? Quas porro saepe suscipit aliquid fugiat ab exercitationem officia voluptatibus inventore vel! Expedita consequatur sint autem dicta, modi ab?
-    Obcaecati culpa quasi quo itaque? Fugit beatae dolor laudantium id fuga cum, nostrum nemo ipsum quod alias ratione, ullam, ducimus aperiam ut error iure excepturi aliquam fugiat obcaecati dolorem minus?
-    Facilis possimus vitae itaque repellendus corrupti. Molestiae modi quas sint, dicta dolorem blanditiis aliquid repellat delectus dolores fugit. Repudiandae, esse! Quo, eos excepturi modi rem doloribus ad beatae incidunt tempore?
-    Numquam laboriosam iste dolorum voluptatum molestiae unde, adipisci maiores quidem quae beatae similique sunt qui optio esse quos et excepturi, animi obcaecati cum odit earum! Qui explicabo quod dignissimos accusantium.
-    Maxime nostrum quis provident placeat cumque, aut quidem ipsum? Fugiat tenetur dolor earum, id distinctio nihil nostrum eaque eius consectetur, ab recusandae iusto odio et fugit?</p>
+    <p class="lead"> {{post.content}}</p>
+
+    <hr>
+    <h5> Slug : {{ post.slug }}</h5>
+
 
 
 </template>
 <script>
 export default {
-    props: ['id', 'slug']    
+    props: ['id', 'slug'],
+    data() {
+        return {
+            post:[]
+        }
+    },
+    mounted() {
+            fetch("http://localhost:3000/posts/"+this.id).then( r => r.json() )
+            .then( data => this.post = data)
+            .catch( err => console.log(err))
+    },    
 }
 </script>
 <style lang="">
